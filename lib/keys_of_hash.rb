13057f393dea6bd7deea88283d38f6fb17ba1e02
Isa Levine <isaacdlevine@gmail.com>
2019-01-24 04:29:53 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,5 +2,8 @@ class Hash
   def keys_of(*arguments)
     array_output = []
     animals_hash = animals
+    animals_hash.each do |animal_key, country_value|
+      array_output << animal_key
   end
+  array_output
 end
