fab0a9cf7683bd361923e90aa9a68f7ea439db30
Isa Levine <isaacdlevine@gmail.com>
2019-01-24 04:38:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     array_output = []
     self.each do |the_key, the_value|
-      if the_value = arguments
+      if the_value == arguments
         array_output << the_key
       end
     end
